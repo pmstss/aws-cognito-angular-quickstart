7e536705f246e5fc78ca17858dbebcfdcd6c5c5e
Viachaslau <pmstss@gmail.com>
2019-11-28 16:34:45 +0300

    custom config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,21 +3,20 @@ export const environment = {
 
     region: 'us-east-1',
 
-    identityPoolId: 'us-east-1:fbe0340f-9ffc-4449-a935-bb6a6661fd53',
-    userPoolId: 'us-east-1_PGSbCVZ7S',
-    clientId: 'hh5ibv67so0qukt55c5ulaltk',
+    identityPoolId: 'us-east-1:beefe203-1716-417e-9423-1d5d38704b74',
+    userPoolId: 'us-east-1_yVvLDUUeh',
+    clientId: '6s75ehvbok1sg1gc8vl1avnqvg',
 
     rekognitionBucket: 'rekognition-pics',
     albumName: "usercontent",
     bucketRegion: 'us-east-1',
 
-    ddbTableName: 'LoginTrail',
+    ddbTableName: 'table-mycognito',
 
     cognito_idp_endpoint: '',
     cognito_identity_endpoint: '',
     sts_endpoint: '',
     dynamodb_endpoint: '',
     s3_endpoint: ''
-
 };
 
